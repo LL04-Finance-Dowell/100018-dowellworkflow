dd1e74a07479d3d78cf39dc5e40383ab5cbaef4c
Rahultadola <45748425+Rahultadola@users.noreply.github.com>
2022-01-18 12:04:09 +0530

    Update views.py

--- docedit/chat/views.py
+++ docedit/chat/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
-
-from .models import Message, Conversation
+from .models import Room, Message
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here
+def home(request):
+    return render(request, "basicChat/chat.html")
 
-def chat_index(request):
-    return render(request, 'chat/c_index.html', {})
+
+
+def send(request):
+    if request.method=="POST":
+        message = request.POST.get('message')
+        username = request.POST.get('username')
+        room=request.POST.get('room')
+        
+        print(room)
+        if Room.objects.filter(name=room).exists():
+            print("Confirmed, Room exists")
+            new_message = Message(value=message, user=username, room=room)
+            new_message.save()
+            
+        else:
+            new_room = Room(name=room)
+            new_room.save()
+            new_message = Message(value=message, user=username, room=room)
+            new_message.save()   
+    
+
+def getMessages(request,  room):
+    messages = Message.objects.filter(room=room)
+    return JsonResponse({"messages": list(messages.values())})
+
