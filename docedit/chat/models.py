d55d23f1c945226086bfca968a1b55ef07cf298b
Rahultadola <45748425+Rahultadola@users.noreply.github.com>
2022-01-18 12:03:36 +0530

    Update models.py

--- docedit/chat/models.py
+++ docedit/chat/models.py
@@ -1,12 +1,15 @@
 from django.db import models
-from accounts.models import CustomUser
+from datetime import datetime
 
-# Create your models here.
+
+class Room(models.Model): #Acts like a group
+    name = models.CharField(max_length=1000)
 
 class Message(models.Model):
-    content         = models.TextField(max_length=500)
-    sent_by         = models.ForeignKey(CustomUser, on_delete=models.CASECADE)
+    value = models.CharField(max_length=10000000)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    room = models.CharField(max_length=1000000) #ForeignKey(Room, on_delete=models.CASCADE)
+    user = models.CharField(max_length=1000000, default="Anonymous") #use foreignkey for real app
 
-
-class Conversation(models.Model):
-    messages		= models.ManyToManyField(Message, blank=True)
+    def __str__(self):
+        return str(self.value) + ':' + str(self.user) +':' + str(self.room)
