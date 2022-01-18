5e43cbbb35c198d6a304fb57583c76d5bd876635
Rahultadola <45748425+Rahultadola@users.noreply.github.com>
2022-01-18 12:17:47 +0530

    Add files via upload

--- docedit/pages/urls.py
+++ docedit/pages/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import HomePageView
+from django.contrib.auth.decorators import login_required
+
+from .views import homePageView
+
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
+    path('', login_required(homePageView), name='home'),
 ]
--- docedit/pages/views.py
+++ docedit/pages/views.py
@@ -1,4 +1,19 @@
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect #, get_object_or_404
+from organization.models import Organization
 
-class HomePageView(TemplateView):
-	template_name = 'home.html'
+def homePageView(request, *args, **kwargs):
+    org_list = Organization.objects.all()
+    is_member = False
+    for org in org_list :
+        if (request.user in org.staff_members.all()) or (request.user in org.members.all()):
+            print(org)
+            is_member = True
+
+    if is_member :
+        return render(request, 'home.html', {'org_id': org.id})
+    else:
+        return redirect('organization:create-orgniz')
+
+
+#class HomePageView(TemplateView):
+#	template_name = 'home.html'     return render(request, 'home.html', {})
